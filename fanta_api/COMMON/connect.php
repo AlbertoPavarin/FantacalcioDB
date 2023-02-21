8f791c616739fc7c8852c36e9abfadc916e65a58
AlbertoPavarin <albertopavarin@libero.it>
2023-02-21 22:43:58 +0100

    Connect Database

---
+++ fanta_api/COMMON/connect.php
@@ -0,0 +1,27 @@
+<?php
+class Database
+{
+    //credentials localhost
+    private $server = "localhost";
+    private $user = "root";
+    private $passwd = "";
+    private $db = "fantacalcio_db";
+
+    //common credentials
+    private $port = "3306";
+    public $conn;
+
+    public function connect()
+    {
+        try
+        {
+            $this->conn = new mysqli($this->server, $this->user, $this->passwd, $this->db, $this->port);
+        }
+        catch (Exception $ex)
+        {
+            die("Error connecting to database $ex\n\n");
+        }
+        return $this->conn;
+    }
+}
+?>
